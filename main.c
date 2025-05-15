81720361057b680fc8df5a0e28d5800b95701029
Børge Lundsaunet <mail@blund.site>
2025-05-16 01:01:03 +0200

    clean up reading in stencils

--- main.c
+++ main.c
@@ -6,6 +6,12 @@
 
 #include "stencil.h"
 
+typedef struct {
+  uint8_t *code;
+  int write_head;
+  int capacity;
+} ExecutableMemory;
+
 // Functions used for testing cps functionlaity
 void print_result(uintptr_t stack, int result) {
   printf("From continuation passing: %d\n", result);
@@ -13,7 +19,32 @@ void print_result(uintptr_t stack, int result) {
 void choice_a(uintptr_t stack) { puts("a!"); }
 void choice_b(uintptr_t stack) { puts("b!"); }
 
-// Used to patch a hole in the stencil.
+// Read in binary stencil file to memory
+Stencil read_stencil(char* file_path) {
+  char* raw;
+  int file_size = read_file(file_path, &raw);
+
+  // Read footer values from memory
+  uint32_t *footer = (uint32_t *)(raw + file_size - (8*sizeof(Hole) + 4 + 4 + 4));
+
+  // Verify footer
+  uint32_t blur_tag = footer[0];
+  if (blur_tag != 0x72756C62) {
+    fprintf(stderr, "Invalid blur tag!\n");
+    free(raw);
+    exit(-1); // @TODO - :)
+  }
+
+  // Read in structure
+  Stencil stencil;
+  stencil.code = (uint8_t*)raw;
+  stencil.code_size = footer[1];
+  stencil.num_holes = footer[2];
+  memcpy(&stencil.holes, &footer[3], sizeof(Hole) * 4);
+  return stencil;
+}
+
+// Patch a hole in a stencil
 void patch_hole(Stencil* s, int index, uint64_t value) {
   {
     Hole h = s->holes[index];
@@ -24,12 +55,6 @@ void patch_hole(Stencil* s, int index, uint64_t value) {
     }
   }
 }
-
-typedef struct {
-  uint8_t *code;
-  int write_head;
-  int capacity;
-} ExecutableMemory;
 
 ExecutableMemory make_executable_memory() {
   // Allocate a buffer with RWX permissions (on Linux)
@@ -52,27 +77,9 @@ void copy_stencil(ExecutableMemory *em, Stencil *s) {
   em->write_head+=s->code_size;
 }
 
+
 int main() {
-  char* raw;
-  int file_size = read_file("generated/stencils/if_test.bin", &raw);
-
-  // Read footer values from memory
-  uint32_t *footer = (uint32_t *)(raw + file_size - (8*sizeof(Hole) + 4 + 4 + 4));
-
-  // Verify footer
-  uint32_t blur_tag = footer[0];
-  if (blur_tag != 0x72756C62) {
-    fprintf(stderr, "Invalid blur tag!\n");
-    free(raw);
-    return 1;
-  }
-
-  // Read in structure
-  Stencil stencil;
-  stencil.code = (uint8_t*)raw;
-  stencil.code_size = footer[1];
-  stencil.num_holes = footer[2];
-  memcpy(&stencil.holes, &footer[3], sizeof(Hole)*4);
+  Stencil stencil = read_stencil("generated/stencils/if_test.bin");
 
   // Patch the holes!
   uint64_t val1 = (uint64_t)choice_a;
@@ -93,7 +100,7 @@ int main() {
   func(stack, 0);
 
   // Clean up
-  free(raw);
+  free(stencil.code);
   munmap(em.code, em.capacity);
 
   return 0;
